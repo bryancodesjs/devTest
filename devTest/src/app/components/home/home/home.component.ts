b1036f3f1a8a473459662f8dc08ee073ffcd7c57
Bryan <100053356@p.uapa.edu.do>
2023-03-14 17:15:45 -0400

    updating both buy/sell order books

--- devTest/src/app/components/home/home/home.component.ts
+++ devTest/src/app/components/home/home/home.component.ts
@@ -19,39 +19,78 @@ export class HomeComponent implements OnInit {
     this.initWorker();
     // setTimeout(() => {
     //   this.socket.unsubscribe();
-    // }, 3000)
+    // }, 10000)
   }
 
 
   initWorker() {
     this.socket.subscribe((res) => {
       let obj: any = res;
+      let data = obj?.data;
       console.log(obj);
-      if(obj?.action === 'partial' && obj?.table === 'orderBookL2_25'){
-          console.log('full data', obj?.data)
+      if(obj?.table === 'orderBookL2_25'){
+        switch(obj?.action) {
+          case 'partial': 
           obj?.data.map((row:any) => {row.side == 'Sell' ? this.sellSide.push(row) : this.buySide.push(row)});
-      } else if(obj?.action === 'update' && obj?.table === 'orderBookL2_25') {
-        //find matching rows and update them
-        let data = obj?.data;
-        for(let i = 0; i < data.length ; i++) {
-          if(data[i].side == 'Sell') {
-            let refId = data[i].id;
-            this.sellSide.map((row:any) => {
-              if(row.id == refId) {
-                console.log('from price ' + row.price);
-                console.log('to ' + data[i].price);
-                row.price = data[i].price != null && data[i].price != undefined ? data[i].price : row.price;
-                row.size = data[i].size != null && data[i].size != undefined ? data[i].size : row.size;
-                // this.sellSide.splice(this.sellSide.indexOf(row), 1, data[i])
-              }
-              // row.id == refId ? this.sellSide.splice(this.sellSide.indexOf(row), 1, data[i]) : ;
-            })
+          break;
+
+          case 'update':
+          for(let i = 0; i < data.length ; i++) {
+            if(data[i].side == 'Sell') {
+              let refId = data[i].id;
+              this.sellSide.map((row:any) => {
+                if(row.id == refId) {
+                  row.price = data[i].price != null && data[i].price != undefined ? data[i].price : row.price;
+                  row.size = data[i].size != null && data[i].size != undefined ? data[i].size : row.size;
+                }
+              })
+            } else if (data[i].side == 'Buy') {
+              let refId = data[i].id;
+              this.buySide.map((row:any) => {
+                if(row.id == refId) {
+                  row.price = data[i].price != null && data[i].price != undefined ? data[i].price : row.price;
+                  row.size = data[i].size != null && data[i].size != undefined ? data[i].size : row.size;
+                }
+              })
+            }
+          } 
+          console.log('update');
+          break;
+
+          case 'delete':
+          for(let i = 0; i < data.length ; i++) {
+            if(data[i].side == 'Sell') {
+              let refId = data[i].id;
+              this.sellSide.map((row:any) => {
+                if(row.id == refId) {
+                  this.sellSide.splice(this.sellSide.indexOf(row), 1);
+                }
+              })
+            } else if(data[i].side == 'Buy') {
+              let refId = data[i].id;
+              this.buySide.map((row:any) => {
+                if(row.id == refId) {
+                  this.buySide.splice(this.buySide.indexOf(row), 1);
+                }
+              })
+            }
           }
-        } 
-        console.log('update');
-      }
-        
-      },
+          break;
+
+          case 'insert':
+          for(let i = 0; i < data.length ; i++) {
+            if(data[i].side == 'Sell') {
+              this.sellSide.push(data[i]);
+            } else if(data[i].side == 'Buy') {
+              this.buySide.push(data[i]);
+            }
+          }
+          break;
+          
+          default:
+            break;
+        }
+      }},
       (e) => {
         console.error('error:', e);
       },
